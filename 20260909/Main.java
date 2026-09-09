f8db5fadfda6f81757408fb4a79c2dea571921b5
ElonFreeman <zhanghao112358@outlook.com>
2026-09-09 20:13:44 +0800

    26090902

---
+++ 20260909/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("JAVA喵喵喵~~~");
+    }
+}
+
+class triangle
+{
+    static int height=10,width=20;
+
+    void volume()
+    {
+        
+    }
+
+    void square()
+    {
+
+    }
+}
